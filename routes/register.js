2c3bdfac5839d116f6a42b05a690b208051705d8
Lynn <lturnbul@umich.edu>
2017-02-09 14:47:31 -0500

    I added code to the stylesheet "style" and added an images folder, added code to the header page and register page.

--- routes/register.js
+++ routes/register.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
     res.render('register', { title: 'register' });
 });
 
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"
 module.exports = router;
